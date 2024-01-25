d90790ea800165fd8db342f6fb2d47166f760c86
Luiz Henrique Cassettari <ricaun@users.noreply.github.com>
2024-01-25 15:32:17 -0300

    Add `DockablePaneDocumentData` and `DockablePaneExtension`

---
+++ RevitAddin.Dockable.Example/Services/DockablePaneExtension.cs
@@ -0,0 +1,49 @@
+﻿using Autodesk.Revit.UI;
+
+namespace RevitAddin.Dockable.Example.Services
+{
+    /// <summary>
+    /// DockablePaneExtension
+    /// </summary>
+    public static class DockablePaneExtension
+    {
+        /// <summary>
+        ///  Try to show the pane that is not currently visible or in a tab, display the pane in the Revit
+        ///  user interface at its last docked location.
+        /// </summary>
+        /// <param name="dockablePane"></param>
+        /// <remarks>If <paramref name="dockablePane"/> is null or <see cref="Autodesk.Revit.UI.DockablePane.IsShown"/>, nothing happen.</remarks>
+        public static void TryShow(this DockablePane dockablePane)
+        {
+            if (dockablePane is null)
+                return;
+            if (dockablePane.IsShown() == false)
+                dockablePane.Show();
+        }
+
+        /// <summary>
+        /// Try to hide the pane is on screen, hide it. Has no effect on built-in Revit dockable panes.
+        /// </summary>
+        /// <param name="dockablePane"></param>
+        /// <remarks>If <paramref name="dockablePane"/> is null or not <see cref="Autodesk.Revit.UI.DockablePane.IsShown"/>, nothing happen.</remarks>
+        public static void TryHide(this DockablePane dockablePane)
+        {
+            if (dockablePane is null)
+                return;
+            if (dockablePane.IsShown())
+                dockablePane.Hide();
+        }
+
+        /// <summary>
+        /// Returns the current title (a.k.a. window caption) of the dockable pane.
+        /// </summary>
+        /// <param name="dockablePane"></param>
+        /// <remarks>If <paramref name="dockablePane"/> is null, <see cref="string.Empty"/> is return.</remarks>
+        public static string TryGetTitle(this DockablePane dockablePane)
+        {
+            if (dockablePane is null)
+                return string.Empty;
+            return dockablePane.GetTitle();
+        }
+    }
+}
---
+++ RevitAddin.Dockable.Example/Services/IDockablePaneDocumentProvider.cs
@@ -0,0 +1,56 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System.Windows;
+
+namespace RevitAddin.Dockable.Example.Services
+{
+    /// <summary>
+    /// IDockablePaneDocumentProvider
+    /// </summary>
+    public interface IDockablePaneDocumentProvider
+    {
+        /// <summary>
+        /// This method is called when the DockablePane change state. When the visibility of the DockablePane changed, or a document changed inside Revit UI.
+        /// </summary>
+        /// <param name="data"></param>
+        /// <remarks>This method is executed inside the <see cref="Autodesk.Revit.UI.UIApplication.Idling"/> event.</remarks>
+        void DockablePaneChanged(DockablePaneDocumentData data);
+    }
+
+    /// <summary>
+    /// DockablePaneDocumentData
+    /// </summary>
+    public sealed class DockablePaneDocumentData
+    {
+        /// <summary>
+        /// DockablePaneDocumentData
+        /// </summary>
+        /// <param name="dockablePaneId"></param>
+        /// <param name="frameworkElement"></param>
+        /// <param name="document"></param>
+        /// <param name="dockablePane"></param>
+        DockablePaneDocumentData(DockablePaneId dockablePaneId, FrameworkElement frameworkElement, Document document, DockablePane dockablePane)
+        {
+            DockablePaneId = dockablePaneId;
+            FrameworkElement = frameworkElement;
+            Document = document;
+            DockablePane = dockablePane;
+        }
+        /// <summary>
+        /// DockablePaneId related to the DockablePane.
+        /// </summary>
+        public DockablePaneId DockablePaneId { get; }
+        /// <summary>
+        /// The FrameworkElement that is used to display the DockablePane.
+        /// </summary>
+        public FrameworkElement FrameworkElement { get; }
+        /// <summary>
+        /// Document, this could be null if no document is opened.
+        /// </summary>
+        public Document Document { get; }
+        /// <summary>
+        /// DockablePane related to the DockablePaneId, this could be null if no document is opened.
+        /// </summary>
+        public DockablePane DockablePane { get; }
+    }
+}
